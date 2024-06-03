2cf3ddc12e87e08f0c62552719c2b888c3024201
wuhao <wuhao@veiban.com>
2024-06-03 15:40:37 +0800

    f-day05

--- transform.py
+++ transform.py
@@ -15,12 +15,47 @@ def temperature_converter(temp, unit):
         return (temp - 32) * 1.8
     else:
         return "Invalid unit"
-
+    
 # Test the function
 print(temperature_converter(20, "C")) # Output: 68.0
 print(temperature_converter(68, "F")) # Output: 20.0
 print(temperature_converter(30, "K")) # Output: Invalid unit
+    
+def in_cm_transform(len, unit):
+    """
+    This function is used to convert length from inches to centimeters or reverse
+    """
+    if unit == "in" or unit == "inches" or unit == "inch" or unit == "英寸":
+        return len * 2.54
+    elif unit == "cm" or unit == "centimeters" or unit == "centimeter" or unit == "厘米":
+        return len / 2.54
+    else:
+        return "Invalid unit"
+ 
 
+
+# Test the function
 f = float(input("Enter a temperature in Fahrenheit: "))
 c = temperature_converter(f, "F")
-print(f"{f:.1f}华氏度 is equivalent to {c:.1f}摄氏度")
+print(f"{f:.1f}华氏度 is equivalent to {c:.1f}摄氏度")   
+    
+    
+def percent_to_grade(score):
+    """
+    This function is used to convert a score to a letter grade
+    """
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "E"
+
+# Test the function
+score = float(input("Enter a score: "))
+grade = percent_to_grade(score)
+print(f"The grade is {grade}")
---
+++ triangle.py
@@ -0,0 +1,67 @@
+"""
+this is a triangle program
+"""
+
+invalid_triangle_txt: str = "not a valid triangle"
+
+
+def is_valid_triangle(a: int | float, b: int | float, c: int | float) -> bool:
+    """
+    This function checks if the given sides of a triangle are valid or not.
+    """
+    if a + b > c and b + c > a and c + a > b:
+        return True
+    else:
+        return False
+
+
+def triangle_type(a: int | float, b: int | float, c: int | float) -> str:
+    """
+    This function returns the type of triangle based on the given sides of a triangle.
+    """
+    if is_valid_triangle(a, b, c):
+        if a == b == c:
+            return "equilateral"
+        elif a == b or b == c or a == c:
+            return "isosceles"
+        else:
+            return "scalene"
+    else:
+        return invalid_triangle_txt
+
+
+# Test the function
+print(is_valid_triangle(3, 4, 5))  # True
+print(is_valid_triangle(1, 2, 3))  # False
+print(triangle_type(3, 4, 5))  # scalene
+
+
+def triangle_area(a: int | float, b: int | float, c: int | float):
+    """
+    This function returns the area of a triangle based on the given sides of a triangle.
+    """
+    if is_valid_triangle(a, b, c):
+        s = (a + b + c) / 2
+        return (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    else:
+        return invalid_triangle_txt
+
+
+# Test the function
+print(triangle_area(3, 4, 5))  # 6.0
+print(triangle_area(1, 2, 3))  # not a valid triangle
+
+
+def triangle_perimeter(a: int | float, b: int | float, c: int | float):
+    """
+    This function returns the perimeter of a triangle based on the given sides of a triangle.
+    """
+    if is_valid_triangle(a, b, c):
+        return a + b + c
+    else:
+        return invalid_triangle_txt
+
+
+# Test the function
+print(triangle_perimeter(3, 4, 5))  # 12
+print(triangle_perimeter(1, 2, 3))  # not a valid triangle
